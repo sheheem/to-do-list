9902d98ce12d5793bffeb431d51dd1dbdf46137f
sheheem <sheheemzainudeen@gmail.com>
2023-01-17 19:45:03 +0530

    todo initialised

---
+++ src/app/todo/todo.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import {FormGroup, FormBuilder, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.scss']
+})
+export class TodoComponent implements OnInit {
+
+  todoForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+      this.todoForm = this.fb.group({
+        item: ['', Validators.required]
+      });
+  }
+
+}
